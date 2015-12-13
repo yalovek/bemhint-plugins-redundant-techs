be2e80f17d8545971c7987f4879e2cc1a049106f
Vadim Yalovenko <yalovek@ya.ru>
2015-12-13 14:01:48 +0300

    Optimization

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-module.exports = {
-    forEntityTech: function (tech, techsConfig, entity) {
-        var error = {
-            msg: 'Redundand tech',
-            tech: tech.tech
-        };
-
-        entity.addError(error);
-    }
+module.forEntityTech = function (tech, techsConfig, entity) {
+    entity.addError({
+        msg: 'Redundand tech',
+        tech: tech.tech
+    });
 };
